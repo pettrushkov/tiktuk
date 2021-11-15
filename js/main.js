824b9c2ebf63764c6833458a4f8ced443c136ce6
Denis Petrushkov <dpetrushkov@markupus.tech>
2021-11-15 18:10:34 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 import { ApiAccess } from './api.js';
 
 getTrandingFeed();
-// getUserFeed();
-// getUserInfo();
 
 const feedSpinner = document.getElementById('feed-loader'),
   app = document.getElementById('app');
@@ -19,27 +17,31 @@ function outputFeed(data) {
     hideFeedSpinner();
 
     data.forEach(element => {
+      // create video block
       let post = document.createElement('div');
       post.classList.add('post')
       post.classList.add('uk-padding-small')
       post.classList.add('uk-margin')
       post.classList.add('uk-box-shadow-medium')
 
-      // 1. Создаём новый XMLHttpRequest-объект
+      // create request object
       let xhr = new XMLHttpRequest();
-      // 2. Настраиваем его: GET-запрос по URL /article/.../load
+      // set method end url
       xhr.open('GET', 'https://www.tiktok.com/oembed?url=' + element.webVideoUrl);
-      // 3. Отсылаем запрос
+      // send request
       xhr.send();
-      // 4. Этот код сработает после того, как мы получим ответ сервера
+      // this function work after we get request
       xhr.onload = function () {
-        if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-          alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
-        } else { // если всё прошло гладко, выводим результат
+        if (xhr.status !== 200) { // if status not 200 - error
+          alert(`Error ${xhr.status}: ${xhr.statusText}`); 
+        } else {
 
+          // parse result to JSON
           let result = JSON.parse(xhr.response);
+          // add html-code to video block
           post.innerHTML += `<div class="post__video uk-margin-bottom">${result.html}</div>`;
 
+          // add authors meta
           post.innerHTML += `<div class="author uk-flex uk-flex-middle uk-margin-bottom">
             <a href="https://www.tiktok.com/@${element.authorMeta.name}" class="author__avatar">
               <img
@@ -49,6 +51,7 @@ function outputFeed(data) {
             <a href="https://www.tiktok.com/@${element.authorMeta.name}" class="author__name">${element.authorMeta.name}</a>
           </div>`;
 
+          // add hastags
           if (element.hashtags.length > 0) {
             let hashtags = document.createElement('ul');
             hashtags.classList.add('hashtags')
@@ -62,20 +65,22 @@ function outputFeed(data) {
             post.appendChild(hashtags);
           }
 
+          // add post meta
           post.innerHTML += `<div class="post__meta uk-flex uk-flex-between">
           <p class="uk-margin-remove-top"><span uk-icon="comments"></span> ${element.commentCount}</p>
           <p class="uk-margin-remove-top"><span uk-icon="heart"></span> ${element.diggCount}</p>
         </div>
           </div>`;
 
+          // add post block to end of #app
           app.appendChild(post);
-
         }
       };
     });
   }
 }
 
+// get feed data
 function getTrandingFeed() {
   const data = null;
 
@@ -86,52 +91,13 @@ function getTrandingFeed() {
     if (this.readyState === this.DONE) {
       console.log('Page TrandingFeed:')
       console.log(JSON.parse(this.responseText));
+      // call function and give parsed in JSON respond to it
       outputFeed(JSON.parse(this.responseText));
     }
   });
 
-  xhr.open("GET", ApiAccess.url);
-  xhr.setRequestHeader("x-rapidapi-host", ApiAccess.host);
-  xhr.setRequestHeader("x-rapidapi-key", ApiAccess.token);
-
-  xhr.send(data);
-}
-
-function getUserFeed() {
-  const data = null;
-
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log('Page UserFeed:')
-      console.log(JSON.parse(this.responseText));
-    }
-  });
-
-  xhr.open("GET", ApiAccess.url);
-  xhr.setRequestHeader("x-rapidapi-host", ApiAccess.host);
-  xhr.setRequestHeader("x-rapidapi-key", ApiAccess.token);
-
-  xhr.send(data);
-}
-
-function getUserInfo() {
-  const data = null;
-
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log('Page UserInfo:')
-      console.log(JSON.parse(this.responseText));
-    }
-  });
-
-  xhr.open("GET", ApiAccess.url);
-  xhr.setRequestHeader("x-rapidapi-host", ApiAccess.host);
+  xhr.open("GET", "https://tiktok33.p.rapidapi.com/trending/feed");
+  xhr.setRequestHeader("x-rapidapi-host", "tiktok33.p.rapidapi.com");
   xhr.setRequestHeader("x-rapidapi-key", ApiAccess.token);
 
   xhr.send(data);
